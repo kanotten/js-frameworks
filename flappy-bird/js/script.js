9f0b8177ec1a65d19b9fec92cb7b030a298d3714
Ludvig Alvir <ludvi@OSL_Ludvig_FED1.res.bitpro.as>
2025-02-13 09:58:37 +0100

    buttons

--- flappy-bird/js/script.js
+++ flappy-bird/js/script.js
@@ -3,8 +3,39 @@ const c = canvas.getContext("2d");
 //Drawuing a box
 //         x   y   Width height
 
+/* console.log(Math.random() > 0.5); // log false and true 50% of the time
+  //Math.random gives a random number from 0 to 0.9999999 */
+
+const player = {
+  x: 0,
+  y: 0,
+  speed: 1,
+};
+
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
-  c.fillRect(Math.random() * 350, 50, 50, 50);
-  console.log(Math.random() * 10);
+  player.x += 10;
+  player.y += 10;
+  c.clearRect(0, 0, 400, 400);
+  c.fillRect(player.x, player.y, 50, 50);
 });
+
+const left = document.querySelector("#left");
+const rigth = document.querySelector("#right");
+
+left.addEventListener("click", () => {
+  player.speed = -1;
+});
+rigth.addEventListener("click", () => {
+  player.speed = 1;
+});
+
+setInterval(() => {
+  render();
+}, 20);
+
+function render() {
+  player.x += player.speed;
+  c.clearRect(0, 0, 400, 400);
+  c.fillRect(player.x, player.y, 50, 50);
+}
